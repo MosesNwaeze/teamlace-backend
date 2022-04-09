72460b24344b4b9d765fbf31244528f2487fe06c
Moses Nwaeze <ogbonnasmart@gmail.com>
2022-04-09 08:13:31 +0100

    Error: database and tables created automatically

--- configurations/database_connection.js
+++ configurations/database_connection.js
@@ -10,9 +10,45 @@ const connectionProperties = {
 const connection = mysql.createConnection(connectionProperties);
 connection.connect((error) => {
   if (error) {
-    console.error(error);
+    console.error(error.message);
     return;
   }
+  //Create a database
+  const sql = `CREATE DATABASE IF NOT EXISTS teamlace;`;
+  connection.query(sql, function (error) {
+    if (error) {
+      console.error(error.message);
+      return;
+    }
+    const queryUser = `CREATE TABLE IF NOT EXISTS user(
+      user_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+      username VARCHAR(200) NOT NULL UNIQUE,
+      password VARCHAR(300) NOT NULL
+      );`;
+
+    connection.query(queryUser, function (error) {
+      if (error) {
+        console.log(error.message);
+        return;
+      }
+    });
+
+    const queryProduct = `CREATE TABLE IF NOT EXISTS product(
+        product_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        product_name VARCHAR(200) NOT NULL,
+        category_id VARCHAR(300) NOT NULL,
+        category_name VARCHAR(300) NOT NULL,
+        unit_price DOUBLE NOT NULL,
+        status VARCHAR(100) NOT NULL,
+        available_since DATE NOT NULL
+        );`;
+
+    connection.query(queryProduct, function (error) {
+      if (error) {
+        console.error(error.message);
+      }
+    });
+  });
 });
 
 module.exports = connection;
